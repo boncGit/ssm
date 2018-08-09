d13c2d629b96121e5d94dea9d3d5c9b343c5da9b
mengxy@asiainfo.com <mengxy@asiainfo.com>
2018-08-09 15:50:24 +0800

    调整代码结构

--- ssm-service/src/main/java/com/watchme/system/login/service/impl/LoginServiceImpl.java
+++ ssm-service/src/main/java/com/watchme/system/login/service/impl/LoginServiceImpl.java
@@ -1,10 +1,12 @@
 package com.watchme.system.login.service.impl;
 
 import com.watchme.system.login.service.ILoginService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Mengxy on 2018/8/8.
  */
+@Service
 public class LoginServiceImpl implements ILoginService{
 
     /**
