8c5b145be72d2a987b79eb7bca759486d3e6266d
mengxy@asiainfo.com <mengxy@asiainfo.com>
2018-08-16 18:00:57 +0800

    加注释

--- ssm-service/src/main/java/com/watchme/common/utils/thread/TestThreadLocal.java
+++ ssm-service/src/main/java/com/watchme/common/utils/thread/TestThreadLocal.java
@@ -4,7 +4,7 @@ package com.watchme.common.utils.thread;
  * Created by Mengxy on 2018/8/16.
  */
 public class TestThreadLocal {
-    // 定义匿名子类创建ThreadLocal的变量
+    // 定义匿名子类创建ThreadLocal的变量  ThreadLocal：与当前线程有关
     private static ThreadLocal<Integer> seqNum = new ThreadLocal<Integer>() {
         // 覆盖初始化方法
         public Integer initialValue() {
