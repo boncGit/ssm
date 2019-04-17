bb805d135d531c6aec552f25531513545e5ec88d
孟祥宇 <xiangyu.meng2@hytera.com>
2019-04-17 17:05:02 +0800

    提交文件

--- ssm-service/src/main/java/com/watchme/Test.java
+++ ssm-service/src/main/java/com/watchme/Test.java
@@ -8,6 +8,6 @@ public class Test {
         String str = "{X_PROCESS_RESULT:\"\",\"X_STATUS\":\"S\",\"X_MESSAGE\":null,\"X_RESERVED1\":\"\",\"X_RESERVED2\":\"\",\"X_RESERVED3\":\"\"}";
         str.replace("\\","");
         JSONObject json = JSONObject.parseObject(str);
-        System.out.println(json);
+        //System.out.println(json);
     }
 }
